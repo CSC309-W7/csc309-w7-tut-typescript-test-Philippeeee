e2127552eeb75de55da5f9f08f2e0ece7dab2de4
Philippe Filiatreault <22filiatreaultphilippe@gmail.com>
2026-02-25 19:39:52 -0500

    finished tut

--- src/users.ts
+++ src/users.ts
@@ -1,15 +1,27 @@
 import type { User } from "./types";
 
-export const apiResponse: unknown = [
+export const apiResponse: any[] = [ // unknown
   { name: "Tony", age: 23 },
   { name: "Kevin", age: "24" }, // invalid
   { name: "Jim", age: 25 },
 ];
 
 export function getUsersData(): User[] {
-  return apiResponse as User[]; // intentionally unsafe
+  // return apiResponse as User[]; // intentionally unsafe
+  const agesInt = formatAges(apiResponse)
+  const tempLst: User[] = []
+  for (let i = 0; i < apiResponse.length; i++) {
+    let user = apiResponse[i]
+    const tempUser: User = {name: "", age: 0}
+    // tempUser.age = Number(user.age.toFixed(0))
+    tempUser.age = Number(agesInt[i])
+    tempUser.name = user.name
+    tempLst.push(tempUser)
+  }  
+
+  return tempLst
 }
 
 export function formatAges(users: User[]): string[] {
-  return users.map((u) => u.age.toFixed(0));
+  return users.map((u) => Number(u.age).toFixed(0));
 }
